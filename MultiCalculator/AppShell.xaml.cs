4b6b0b0a45ddddf3e31e37c8b9f89c4d29bb29d4
Влад Козлов <vkozlov15012005@gmail.com>
2023-10-17 22:38:43 +0300

    Добавьте файлы проекта.

---
+++ MultiCalculator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiCalculator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MultiCalculator/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiCalculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
